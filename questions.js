94606b66e2a88c014394dc3d3cacdea134cf6758
mahalakshmi-2305 <77005088+mahalakshmi-2305@users.noreply.github.com>
2021-06-26 22:04:49 +0530

    First Commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which type of JavaScript language is ___",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following also known as Conditional Expression:",
+    answer: "immediate if",
+    options: [
+      "Alternative to if-else",
+      "Switch statement",
+      "If-then-else statement",
+      "immediate if"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In JavaScript, what is a block of statement?",
+    answer: "block that combines a number of statements into a single compound statement",
+    options: [
+      "Conditional block",
+      "block that combines a number of statements into a single compound statement",
+      "both conditional block and a single statement",
+      "block that contains a single statement"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The 'function' and 'var' are known as:",
+    answer: "Declaration statements",
+    options: [
+      "Keywords",
+      "Data types",
+      "Declaration statements",
+      "Prototypes"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which of the following variables takes precedence over the others if the names are the same?",
+    answer: "The local element",
+    options: [
+      "Global variable",
+      "The local element",
+      "The two of the above",
+      "None of the above"
+    ]
+  }
+]
